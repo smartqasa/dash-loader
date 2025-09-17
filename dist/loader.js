eae2f3f48a420d9c2e32604c99ef3017d6e3ff09
Tom Bruno <tom.bruno@smartqasa.com>
2025-09-17 00:42:57 -0400

    🔖 Release 6.1.8

--- dist/loader.js
+++ dist/loader.js
@@ -539,5 +539,5 @@ ScreenSaver = __decorate([
 ], ScreenSaver);
 
 window.smartqasa = window.smartqasa || {};
-console.info(`%c SmartQasa Loader ⏏ ${"6.1.8-beta.10"} (Built: ${"2025-09-17T04:42:43.519Z"}) `, "background-color: #0000ff; color: #ffffff; font-weight: 700;");
+console.info(`%c SmartQasa Loader ⏏ ${"6.1.8"} (Built: ${"2025-09-17T04:42:54.871Z"}) `, "background-color: #0000ff; color: #ffffff; font-weight: 700;");
 //# sourceMappingURL=loader.js.map
