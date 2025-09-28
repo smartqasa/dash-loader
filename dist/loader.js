3a05a0cf4d5ab9c4ba97a15116718c31e3d70312
Tom Bruno <tom.bruno@smartqasa.com>
2025-09-28 07:28:42 -0400

    🔖 Beta release 6.1.20-beta.1

--- dist/loader.js
+++ dist/loader.js
@@ -631,5 +631,5 @@ window.smartqasa = window.smartqasa || {};
 window.addEventListener("unhandledrejection", (event) => {
     console.error("[LOADER] Unhandled promise rejection:", event.reason);
 });
-console.info(`%c SmartQasa Loader ⏏ ${"6.1.19-beta.3"} (Built: ${"2025-09-28T11:26:31.334Z"}) `, "background-color: #0000ff; color: #ffffff; font-weight: 700;");
+console.info(`%c SmartQasa Loader ⏏ ${"6.1.20-beta.1"} (Built: ${"2025-09-28T11:28:39.495Z"}) `, "background-color: #0000ff; color: #ffffff; font-weight: 700;");
 //# sourceMappingURL=loader.js.map
