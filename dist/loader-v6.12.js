f621fd101ef9c356f7421f37f18a560ba5f2a1e7
Tom Bruno <tom.bruno@smartqasa.com>
2026-04-14 18:43:20 -0400

    🔧 Update version to 6.2.6-beta.2 in package.json and package-lock.json

--- dist/loader-v6.12.js
+++ dist/loader-v6.12.js
@@ -4186,5 +4186,5 @@ if (window.fully) {
     console.log('Device Model: ' + window.fully.getDeviceModel());
     window.smartqasa.deviceModel = window.fully.getDeviceModel();
 }
-window.smartqasa.versionLoader = "6.2.6-beta.1";
-console.info('%c SmartQasa Loader ⏏ ' + "6.2.6-beta.1" + ' ', 'background-color: #0000ff; color: #ffffff; font-weight: 700;');
+window.smartqasa.versionLoader = "6.2.6-beta.2";
+console.info('%c SmartQasa Loader ⏏ ' + "6.2.6-beta.2" + ' ', 'background-color: #0000ff; color: #ffffff; font-weight: 700;');
